b031320f276cff44d84175184ecd6810c7260c7a
vitormarinheiro1 <vitormarinheiro9@gmail.com>
2024-06-21 09:36:40 -0300

    style(Footer): nova estilização para o rodape

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,12 +1,18 @@
 import React from "react"
+import { FaLinkedin, FaGithub } from "react-icons/fa";
 
 export function Footer() {
     return (
-        <footer className="footer  border-l-transparent border-r-transparent text-white bg-gradient-to-tr from-black to-gray-900">
-            <div className='container p-12 flex justify-between'>
-                <span>Vitor Marinheiro</span>
-                <p>All rights reserved.</p>
-            </div>
+        <footer className="h-full text-gray-300 p-4 bg-gray-900 text-center">
+            <ul className="flex justify-center w-1/10 mx-auto my-4 gap-2">
+                <li className="list-none gap-2 hover:text-white">
+                    <a href="https://br.linkedin.com/in/vitor-marinheiro-590a461ba" target="_blank"><FaLinkedin size={20} /></a>
+                </li>
+                <li className="list-none gap-2 hover:text-white">
+                    <a href="https://github.com/vitormarinheiro1" target="_blank"><FaGithub size={20} /></a>
+                </li>
+            </ul>
+            <p className="mb-3 text-sm font-headline font-semibold">© 2024 Desenvolvido por Vitor Marinheiro | Todos os direitos reservados.</p>
         </footer>
     )
 }
